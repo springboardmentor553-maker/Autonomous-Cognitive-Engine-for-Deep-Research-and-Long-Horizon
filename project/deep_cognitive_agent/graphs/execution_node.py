3fadfd492c8a72e02fa1743c4f7eb8ba7524a4bf
porwalsarvagya09 <porwalsarvagya09@gmail.com>
2026-02-28 16:14:55 +0530

    Add executor node for step execution

--- project/deep_cognitive_agent/graphs/execution_node.py
+++ project/deep_cognitive_agent/graphs/execution_node.py
@@ -1,15 +1,45 @@
-from typing import List, Dict, TypedDict
+from langchain_google_genai import ChatGoogleGenerativeAI
+from graphs.execution_state import ExecutionState
+import os
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+)
 
 
-class ExecutionState(TypedDict):
+def execute_step(state: ExecutionState) -> ExecutionState:
     """
-    This represents the full execution memory of the agent.
-    Every node in the graph will read and modify this state.
+    Executes one step from the TODO list.
     """
 
-    task: str
-    todos: List[Dict]
-    current_step: int
-    step_outputs: List[str]
-    reflection_notes: List[str]
-    final_answer: str
+    todos = state["todos"]
+    current_index = state["current_step"]
+
+    # If all steps are completed, return state
+    if current_index >= len(todos):
+        return state
+
+    step_text = todos[current_index]["task"]
+
+    print(f"\nExecuting Step {current_index + 1}: {step_text}")
+
+    response = llm.invoke(
+        f"""
+You are executing a step from a structured plan.
+
+Step:
+{step_text}
+
+Provide a detailed and precise execution output.
+"""
+    )
+
+    # Store output
+    state["step_outputs"].append(response.content)
+
+    # Move to next step
+    state["current_step"] += 1
+
+    return state
